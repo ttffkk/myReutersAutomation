9fb005684df5e81daef60fb8215006833e2728f0
ttffkk <tfkaminski@gmail.com>
2023-09-29 19:04:48 +0200

    Blanko and Delivery work

--- Delivery.py
+++ Delivery.py
@@ -1,14 +1,48 @@
 import subprocess
+import sys
+import os
 
-# Replace 'your_command_here' with the command you want to run in CMD.
-command = 'aws polly start-speech-synthesis-task --engine standard --language-code en-US --output-format mp3 --output-s3-bucket-name reuters-mp3-readout --output-s3-key-prefix  --text-type text --voice-id Joanna'
+# Check if the correct number of arguments are provided
+if len(sys.argv) != 3:
+    print("Usage: python Delivery.py h1_cleaned filename")
+    sys.exit(1)
+
+# Get the arguments
+h1_cleaned = sys.argv[1]
+filename = sys.argv[2]
+
+# Construct the local file path
+local_file_path = os.path.join(filename)
+
+# Construct the AWS CLI command as a list of strings
+aws_command = [
+    'aws',
+    'polly',
+    'start-speech-synthesis-task',
+    '--engine',
+    'standard',
+    '--language-code',
+    'en-US',
+    '--output-format',
+    'mp3',
+    '--output-s3-bucket-name',
+    'reuters-mp3-readout',
+    '--output-s3-key-prefix',
+    h1_cleaned,  # Use h1_cleaned directly
+    '--text-type',
+    'text',
+    '--voice-id',
+    'Joanna',
+    '--text',
+    f'file://{local_file_path}'  # Use 'file://' to specify a local file
+]
 
 # Run the command in CMD.
-result = subprocess.run(command, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+result = subprocess.run(aws_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # Print the output and errors, if any.
 print("Output:")
-print(result.stdout)
+print(result.stdout.decode('utf-8'))  # Decode the output as UTF-8
 
 print("Errors:")
-print(result.stderr)
+print(result.stderr.decode('utf-8'))  # Decode the error messages as UTF-8
--- blankocleaner.py
+++ blankocleaner.py
@@ -1,61 +1,70 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import subprocess
 
 # Function to remove special characters and whitespace
 def remove_special_characters(text):
     return ''.join(e for e in text if e.isalnum())
 
-# Input URL from the command line
-url = input("Enter the URL: ")
+# Function to process a single URL
+def process_url(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
 
-# Send a GET request to the URL
-response = requests.get(url)
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the HTML content of the page
-    soup = BeautifulSoup(response.text, 'html.parser')
+        # Find the h1 tag
+        h1_tag = soup.find('h1')
 
-    # Find the h1 tag
-    h1_tag = soup.find('h1')
+        if h1_tag:
+            # Get the text inside the h1 tag
+            h1_text = h1_tag.get_text()
 
-    if h1_tag:
-        # Get the text inside the h1 tag
-        h1_text = h1_tag.get_text()
+            # Remove special characters and whitespace from h1 text
+            h1_cleaned = remove_special_characters(h1_text)
 
-        # Remove special characters and whitespace from h1 text
-        h1_cleaned = remove_special_characters(h1_text)
+            # Create a directory for storing the files if it doesn't exist
+            if not os.path.exists("output"):
+                os.makedirs("output")
 
-        # Create a directory for storing the files if it doesn't exist
-        if not os.path.exists("output"):
-            os.makedirs("output")
+            # Create a file with the h1 tag as the filename
+            filename = os.path.join("output", f"{h1_cleaned}.txt")
 
-        # Create a file with the h1 tag as the filename
-        filename = os.path.join("output", f"{h1_cleaned}.txt")
+            # Find all paragraph tags
+            paragraphs = soup.find_all('p')
 
-        # Find all paragraph tags
-        paragraphs = soup.find_all('p')
+            # Check if any <p> tag contains "Acquire Licensing Rights" and exclude it
+            paragraphs = [p for p in paragraphs if "Acquire Licensing Rights" not in p.get_text()]
 
-        # Check if any <p> tag contains "Acquire Licensing Rights" and exclude it
-        paragraphs = [p for p in paragraphs if "Acquire Licensing Rights" not in p.get_text()]
+            # Combine the h1 text and paragraphs into one string
+            content = h1_text + '\n\n'
+            for paragraph in paragraphs:
+                content += paragraph.get_text() + '\n\n'
 
-        # Combine the h1 text and paragraphs into one string
-        content = h1_text + '\n\n'
-        for paragraph in paragraphs:
-            content += paragraph.get_text() + '\n\n'
+            # Remove content after "Reporting"
+            index = content.find("Reporting")
+            if index != -1:
+                content = content[:index]
 
-        # Remove content after "Reporting"
-        index = content.find("Reporting")
-        if index != -1:
-            content = content[:index]
+            # Write the content to the file
+            with open(filename, 'w', encoding='utf-8') as file:
+                file.write(content)
 
-        # Write the content to the file
-        with open(filename, 'w', encoding='utf-8') as file:
-            file.write(content)
-
-        print(f"Content saved to {filename}")
+            print(f"Content saved to {filename}")
+            subprocess.run(["python", "Delivery.py", h1_cleaned, filename])
+        else:
+            print("No h1 tag found on the page.")
     else:
-        print("No h1 tag found on the page.")
-else:
-    print("Failed to retrieve the web page. Check the URL and try again.")
+        print(f"Failed to retrieve the web page at URL: {url}")
+
+# Read URLs from a file (e.g., urls.txt)
+with open("urls.txt", "r") as url_file:
+    urls = url_file.read().splitlines()
+
+# Process each URL
+for url in urls:
+    process_url(url)
